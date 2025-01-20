769a9da984f8e0c57b727fdd5e0375ae25c89a51
MewXD <97044340+MewXD@users.noreply.github.com>
2025-01-21 01:21:57 +0700

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -21,7 +21,7 @@ end
 
 local DiscordLib = loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
 
-local win = DiscordLib:Window("Fisch-v0.04")
+local win = DiscordLib:Window("Fisch-v0.05")
 
 local serv = win:Server("Main", "")
 
@@ -81,34 +81,49 @@ end
 )
 
 
+local running = false  
+
+local function startAutoEquip()
+    running = true  
+    while running do
+        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
+            local holdingRod = false
+            for _, tool in pairs(LocalPlayer.Character:GetChildren()) do
+                if tool:IsA("Tool") and tool.Name:lower():find("rod") then
+                    holdingRod = true
+                    break
+                end
+            end
+            
+            if not holdingRod then
+                for _, v in pairs(LocalPlayer.Backpack:GetChildren()) do
+                    if v:IsA("Tool") and v.Name:lower():find("rod") then
+                        equipitem(v.Name)
+                        wait(2) 
+                        break
+                    end
+                end
+            end
+        end
+        wait(1) 
+    end
+end
+
+local function stopAutoEquip()
+    running = false  
+end
+
 local tgls = serv:Channel("Auto")
 
 tgls:Toggle(
     "Auto Equip",
     false,
     function(v)
-        while v do
-            if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
-
-                local holdingRod = false
-                for _, tool in pairs(LocalPlayer.Character:GetChildren()) do
-                    if tool:IsA("Tool") and tool.Name:lower():find("rod") then
-                        holdingRod = true
-                        break
-                    end
-                end
-                
-                if not holdingRod then
-                    for _, v in pairs(LocalPlayer.Backpack:GetChildren()) do
-                        if v:IsA("Tool") and v.Name:lower():find("rod") then
-                            equipitem(v.Name)
-                            wait(2) 
-                            break
-                        end
-                    end
-                end
-            end
-            wait(1) 
+        if v then
+            startAutoEquip()  
+        else
+            stopAutoEquip() 
         end
     end
 )
+
