571354e846895460073b8730b2a5ac0b38969f23
MewXD <97044340+MewXD@users.noreply.github.com>
2025-01-24 08:00:10 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -29,7 +29,7 @@ end
 
 local DiscordLib = loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
 
-local win = DiscordLib:Window("Test-v1")
+local win = DiscordLib:Window("Test-v1.1")
 
 local serv = win:Server("Main", "")
 
