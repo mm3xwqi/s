faeb472d395c1c4b20abdf7ede8a8df7ae3e02c9
MewXD <97044340+MewXD@users.noreply.github.com>
2025-01-25 04:46:08 +0700

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -29,7 +29,7 @@ end
 
 local DiscordLib = loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
 
-local win = DiscordLib:Window("Fisch-1.4.3")
+local win = DiscordLib:Window("Fisch-1.4.4")
 
 local serv = win:Server("Main", "")
 
@@ -183,6 +183,11 @@ tgls:Toggle(
     end
 )
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+getgenv().config = getgenv().config or {}
+getgenv().config.auto_shake = getgenv().config.auto_shake or false
+
 tgls:Toggle(
     "auto_shake",
     false,
@@ -190,15 +195,12 @@ tgls:Toggle(
         while getgenv().config.auto_shake do
             task.wait()
 
-            -- Fixing the issue of using 'lp', use 'LocalPlayer' instead
             local playerGui = LocalPlayer:WaitForChild("PlayerGui")
             local shake_button = playerGui:FindFirstChild("shakeui") 
             and playerGui.shakeui:FindFirstChild("safezone") 
             and playerGui.shakeui.safezone:FindFirstChild("button")
 
-            -- You can trigger the shake action here, if button is found
             if shake_button then
-                -- Perform the shake action, e.g. click the button or simulate input
                 VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false, game)
             end
         end
