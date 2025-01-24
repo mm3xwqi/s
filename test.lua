4ad5163c5a339ce3f4d71bea73aa82649f4994dc
MewXD <97044340+MewXD@users.noreply.github.com>
2025-01-25 06:08:26 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,9 @@
-local LocalPlayer = game.Players.LocalPlayer
-local re = game.ReplicatedStorage
-
-
 local Player = game:GetService("Players")
+local LocalPlayer = Player.LocalPlayer
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GuiService = game:GetService("GuiService")
+local Char = LocalPlayer.Character
 
 local islandOptions = {}
 
@@ -14,70 +13,50 @@ for _, teleport_island in pairs(workspace.world.spawns.TpSpots:GetChildren()) do
     end
 end
 
-equipitem = function(v)
+equipitem = function (v)
     if LocalPlayer.Backpack:FindFirstChild(v) then
         local Eq = LocalPlayer.Backpack:FindFirstChild(v)
         LocalPlayer.Character.Humanoid:EquipTool(Eq)
     end
 end
 
-local running = false
-local function startAutoEquip()
-    running = true
-    while running do
-        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
-            local holdingRod = false
-            for _, tool in pairs(LocalPlayer.Character:GetChildren()) do
-                if tool:IsA("Tool") and tool.Name:lower():find("rod") then
-                    holdingRod = true
-                    break
-                end
-            end
-            
-            if not holdingRod then
-                for _, v in pairs(LocalPlayer.Backpack:GetChildren()) do
-                    if v:IsA("Tool") and v.Name:lower():find("rod") then
-                        equipitem(v.Name)
-                        wait(2)
-                        break
-                    end
-                end
-            end
-        end
-        wait(1)
-    end
-end
 
-local function stopAutoEquip()
-    running = false
-end
-
-local rod = LocalPlayer.Character:FindFirstChild("Tool")
+local rod = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Tool")
 if rod and rod:FindFirstChild("events") and rod.events:FindFirstChild("cast") then
     rod.events.cast:FireServer(1, 1)
 end
 
-local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))()
 
-local win = DiscordLib:Window("test8")
+
+local DiscordLib = loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+
+local win = DiscordLib:Window("Fisch-1.4.6")
 
 local serv = win:Server("Main", "")
 
-local tgls = serv:Channel("Auto")
+local btns = serv:Channel("Fising")
 
-tgls:Toggle(
-    "Auto Equip",
-    false,
-    function(v)
-        if v then
-            startAutoEquip()  
-        else
-            stopAutoEquip() 
+btns:Button(
+    "reel-Perfect",
+    function()
+            while true do
+                game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("reelfinished"):FireServer(100, 1)
+                wait(0.1)
+            end
         end
-    end
 )
 
-tgls:Button(
+btns:Button(
+    "reel-NoPerfect",
+    function()
+            while true do
+                game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("reelfinished"):FireServer(100, false)
+                wait(0)
+            end
+        end
+)
+
+btns:Button(
     "Cast",
     function()
         while true do
@@ -93,73 +72,6 @@ tgls:Button(
     end
 )
 
-
-local GuiService = game:GetService("GuiService")
-local UserInputService = game:GetService("UserInputService")
-
-local GuiService = game:GetService("GuiService")
-
-local GuiService = game:GetService("GuiService")
-local VirtualInputManager = game:GetService("VirtualInputManager") 
-
-tgls:Toggle(
-    "Auto Shake", "Navigate", function(state)
-    if state then
-        getgenv().config.auto_shake = true
-
-        
-        spawn(function()
-            while getgenv().config.auto_shake do
-                task.wait()
-
-                
-                local playerGui = LocalPlayer:WaitForChild("PlayerGui")
-                local shake_button = playerGui:FindFirstChild("shakeui") 
-                    and playerGui.shakeui:FindFirstChild("safezone") 
-                    and playerGui.shakeui.safezone:FindFirstChild("button")
-
-                if shake_button then
-                    
-                    shake_button.Selectable = true
-                    GuiService.SelectedObject = shake_button 
-
-                    
-                    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, nil) -- กดปุ่ม Enter
-                    task.wait(0.05)
-                    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, nil) -- ปล่อยปุ่ม Enter
-                end
-            end
-        end)
-    else
-        getgenv().config.auto_shake = false
-    end
-end
-)
-tgls:Toggle(
-    "auto reel", "ToggleInfo", function(state)
-    if state then
-        getgenv().config.auto_reel = true
-
-        spawn(function()
-            while getgenv().config.auto_reel do
-                task.wait(0)  
-
-                
-                local playerGui = LocalPlayer:FindFirstChild("PlayerGui")
-                if playerGui then
-                    local reel = playerGui:FindFirstChild("reel")
-                        if ReplicatedStorage and ReplicatedStorage.events and ReplicatedStorage.events.reelfinished then
-                            local success, errorMsg = pcall(function()
-                                ReplicatedStorage.events.reelfinished:FireServer(100, 1)
-                        end)
-                end
-            end
-        end
-        getgenv().config.auto_reel = false
-    end)
-end
-end
-)
 
 local isToggledOn = false
 local originalSize = nil
@@ -185,7 +97,7 @@ local function restoreOriginalSize()
     end
 end
 
-tgls:Toggle(
+btns:Toggle(
     "legit",
     false,
     function(state)
@@ -203,6 +115,145 @@ tgls:Toggle(
         end
     end
 )
+
+local PlayerGUI = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+btns:Button(
+        "Shake",
+        function()
+            while true do
+            local shakeUI = PlayerGUI:FindFirstChild("shakeui")
+            if shakeUI and shakeUI.Enabled then
+                local safezone = shakeUI:FindFirstChild("safezone")
+                if safezone then
+                    local button = safezone:FindFirstChild("button")
+                    if button and button:IsA("ImageButton") and button.Visible then
+                        GuiService.SelectedObject = button
+                         VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+                    end
+                end
+	wait(0)
+        end
+    end
+end
+)
+
+
+local running = false  
+local function startAutoEquip()
+    running = true  
+    while running do
+        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("Humanoid") then
+            local holdingRod = false
+            for _, tool in pairs(LocalPlayer.Character:GetChildren()) do
+                if tool:IsA("Tool") and tool.Name:lower():find("rod") then
+                    holdingRod = true
+                    break
+                end
+            end
+            
+            if not holdingRod then
+                for _, v in pairs(LocalPlayer.Backpack:GetChildren()) do
+                    if v:IsA("Tool") and v.Name:lower():find("rod") then
+                        equipitem(v.Name)
+                        wait(2) 
+                        break
+                    end
+                end
+            end
+        end
+        wait(1) 
+    end
+end
+
+local function stopAutoEquip()
+    running = false  
+end
+
+local tgls = serv:Channel("Auto")
+
+tgls:Toggle(
+    "Auto-Equip",
+    false,
+    function(v)
+        if v then
+            startAutoEquip()  
+        else
+            stopAutoEquip() 
+        end
+    end
+)
+
+
+
+getgenv().config = getgenv().config or {}
+getgenv().config.auto_shake = getgenv().config.auto_shake or false
+
+tgls:Toggle(
+    "auto_shake",
+    false,
+    spawn(function()
+        while getgenv().config.auto_shake do
+            task.wait(0.5)
+
+            local playerGui = LocalPlayer:WaitForChild("PlayerGui")
+            local shake_button = playerGui:FindFirstChild("shakeui")
+            and shake_button:FindFirstChild("safezone")
+            and shake_button.safezone:FindFirstChild("button")
+
+            if shake_button then
+                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false, game)
+            else
+                print("Shake button not found")
+            end
+        end
+    end)
+)
+
+tgls:Toggle(
+    "Auto-Chest",
+    false,
+    function ()
+            local originalPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            local chest = workspace.ActiveChestsFolder.Pad.Chests:GetChildren()
+            if chest and chest:FindFirstChild("Position") then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = chest.Position
+                wait(2)
+                VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
+                VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.E, false, game)
+                wait(0)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = originalPosition
+            end
+        end
+)
+
+local btns = serv:Channel("Sell")
+
+btns:Button(
+    "SellAll-1Time",
+    function ()
+        game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("SellAll"):InvokeServer()
+    end
+)
+
+btns:Button(
+    "Sell-InHand",
+    function ()
+        game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("Sell"):InvokeServer()
+    end
+)
+
+btns:Button(
+    "SellAll-Loop",
+    function ()
+        while true do
+        game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("SellAll"):InvokeServer()
+        wait(10)
+    end
+end
+)
+
+
 
 local serv = win:Server("Teleport", "")
 
@@ -233,25 +284,6 @@ drops:Button(
     end
 end
 )
-
-local tgls = serv:Channel("Sell")
-
-tgls:Toggle(
-    "Sell-all", "ToggleInfo", function(state)
-    if state then
-        getgenv().config.auto_sell = true
-
-        spawn(function()
-            while getgenv().config.auto_sell do
-                task.wait(0)  
-			game:GetService("ReplicatedStorage"):WaitForChild("events"):WaitForChild("SellAll"):InvokeServer()
-            end
-        getgenv().config.auto_sell = false
-        end)
-    end
-end
-)
-
 
 local btns = serv:Channel("Misc")
 
@@ -376,35 +408,154 @@ pinglabel.TextWrapped = true
 uselessframeone.Name = "uselessframeone"
 uselessframeone.Parent = madebybloodofbatus
 uselessframeone.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-uselessframeone.BackgroundTransparency = 1.000
-uselessframeone.Position = UDim2.new(0.444, 0, 0.252, 0)
-uselessframeone.Size = UDim2.new(0, 16, 0, 17)
+uselessframeone.Position = UDim2.new(0.00444444455, 0, 0.243312627, 0)
+uselessframeone.Size = UDim2.new(0, 224, 0, 5)
 
-UICornerww.CornerRadius = UDim.new(0, 7)
+UICornerww.CornerRadius = UDim.new(0, 50)
+UICornerww.Name = "UICornerww"
 UICornerww.Parent = uselessframeone
 
 uselesslabelfour.Name = "uselesslabelfour"
-uselesslabelfour.Parent = uselessframeone
+uselesslabelfour.Parent = madebybloodofbatus
 uselesslabelfour.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 uselesslabelfour.BackgroundTransparency = 1.000
-uselesslabelfour.Position = UDim2.new(0.597, 0, 0.736, 0)
-uselesslabelfour.Size = UDim2.new(0, 47, 0, 16)
+uselesslabelfour.Position = UDim2.new(0.0580285639, 0, 0.8125, 0)
+uselesslabelfour.Size = UDim2.new(0, 95, 0, 12)
 uselesslabelfour.Font = Enum.Font.SourceSans
-uselesslabelfour.Text = "Made By: Exvn#6765"
+uselesslabelfour.Text = "Anti-Afk Auto Enabled"
 uselesslabelfour.TextColor3 = Color3.fromRGB(255, 255, 255)
 uselesslabelfour.TextSize = 14.000
 
-local userService = game:GetService("UserInputService")
-local rs = game:GetService("RunService")
-local stats = game:GetService("Stats")
 
-getgenv().AntiAfkExecuted = false
 
-rs.RenderStepped:Connect(function()
-if not getgenv().AntiAfkExecuted then
-		game:GetService("VirtualUser"):CaptureController()
-		game:GetService("VirtualUser"):ClickButton1(Vector2.new())
+local Drag = game.CoreGui.thisoneissocoldww.madebybloodofbatus
+gsCoreGui = game:GetService("CoreGui")
+gsTween = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
+local dragging
+local dragInput
+local dragStart
+local startPos
+local function update(input)
+	local delta = input.Position - dragStart
+	local dragTime = 0.04
+	local SmoothDrag = {}
+	SmoothDrag.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	local dragSmoothFunction = gsTween:Create(Drag, TweenInfo.new(dragTime, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), SmoothDrag)
+	dragSmoothFunction:Play()
 end
+Drag.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = Drag.Position
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+Drag.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging and Drag.Size then
+		update(input)
+	end
 end)
 
+
+
+local bbbatusxxxddddd = game:service'VirtualUser'
+
+game:service'Players'.LocalPlayer.Idled:connect(function()
+	bbbatusxxxddddd:CaptureController()
+	bbbatusxxxddddd:ClickButton2(Vector2.new())
 end)
+
+
+
+
+local FPSsLabel = fpslabel
+local RunService = game:GetService("RunService")
+local RenderStepped = RunService.RenderStepped
+local sec = nil
+local FPS = {}
+
+local function fre()
+	local fr = tick()
+	for index = #FPS,1,-1 do
+		FPS[index + 1] = (FPS[index] >= fr - 1) and FPS[index] or nil
+	end
+	FPS[1] = fr
+	local fps = (tick() - sec >= 1 and #FPS) or (#FPS / (tick() - sec))
+	fps = math.floor(fps)
+	fpslabel.Text = fps
+end
+
+
+sec = tick()
+RenderStepped:Connect(fre)
+
+
+
+
+spawn(function()
+	repeat
+		wait(1)
+		local ping = tonumber(game:GetService("Stats"):FindFirstChild("PerformanceStats").Ping:GetValue())
+		ping = math.floor(ping)
+		pinglabel.Text = ping
+
+
+
+	until pinglabel == nil
+end)
+
+local saniye = 0
+
+
+
+local dakika = 0
+
+
+
+local saat = 0
+
+
+
+
+getgenv().zamanbaslaticisi = true
+
+while true do
+
+
+		if getgenv().zamanbaslaticisi then
+
+			saniye = saniye + 1
+
+			wait(1)
+
+		end --if zaman baslaticisi end
+
+
+		if saniye >= 60 then
+			saniye = 0
+			dakika = dakika + 1
+
+		end --if saniye 60 end
+
+
+		if dakika >= 60 then
+			dakika = 0
+			saat = saat + 1
+
+		end --if dakika 60 end
+
+		timerlabel.Text = saat..":"..dakika..":"..saniye
+	end
+    end
+)
