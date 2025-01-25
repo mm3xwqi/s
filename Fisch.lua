0a03cf04117fc0af80abb14efd6dd726e21fee44
MewXD <97044340+MewXD@users.noreply.github.com>
2025-01-25 07:24:45 +0700

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1,6 +1,6 @@
 local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))()
 
-local win = DiscordLib:Window("Fisch-1.5.4f")
+local win = DiscordLib:Window("Fisch-1.5.4f2")
 
 local serv = win:Server("Main", "")
 
