ae5fa7f1f43f982b7bc39266e3e5f2ee5d6f8a58
MewXD <97044340+MewXD@users.noreply.github.com>
2025-08-19 13:40:50 +0700

    Update Prospecting.lua

--- Prospecting.lua
+++ Prospecting.lua
@@ -20,7 +20,7 @@ local walkSpeedValue = humanoid.WalkSpeed
 -- UI Library
 --==================================================
 local DiscordLib = loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
-local win = DiscordLib:Window("MM</>3.2")
+local win = DiscordLib:Window("MM</>3.3")
 local serv = win:Server("Main", "")
 local tgls = serv:Channel("Main")
 local btns = serv:Channel("FastTravel")
@@ -175,17 +175,26 @@ end
 
 -- Find Closest Merchant
 local function findClosestMerchant()
-    local hrp = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-    local closest, closestDist = nil, math.huge
+    local hrp = plr.Character and plr.Character:FindFirstChild("HumanoidRootPart")
+    if not hrp then return nil end
 
+    local closest, closestDist = nil, math.huge
     local npcsFolder = workspace:WaitForChild("NPCs")
+
+    local playerIsland, islandDist = nil, math.huge
     for _, islandFolder in ipairs(npcsFolder:GetChildren()) do
-        local merchant = islandFolder:FindFirstChild("Merchant")
-        if merchant and merchant:FindFirstChild("HumanoidRootPart") then
-            local dist = (hrp.Position - merchant.HumanoidRootPart.Position).Magnitude
-            if dist < closestDist then
-                closest, closestDist = merchant, dist
+        if islandFolder:FindFirstChild("Merchant") and islandFolder.Merchant:FindFirstChild("HumanoidRootPart") then
+            local dist = (hrp.Position - islandFolder.Merchant.HumanoidRootPart.Position).Magnitude
+            if dist < islandDist then
+                playerIsland, islandDist = islandFolder, dist
             end
+        end
+    end
+
+    if playerIsland then
+        local merchant = playerIsland:FindFirstChild("Merchant")
+        if merchant and merchant:FindFirstChild("HumanoidRootPart") then
+            closest = merchant
         end
     end
 
