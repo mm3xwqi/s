849a5b7054242484c668c28b834c118460a4dc08
mm3xwqi <97044340+mm3xwqi@users.noreply.github.com>
2025-10-23 20:14:22 +0700

    Update testanticheat.lua

--- testanticheat.lua
+++ testanticheat.lua
@@ -375,7 +375,7 @@ local function StartAutoReel()
                                 end
                                 
                                 local progress = GetProgress()
-                                if progress and progress >= 0.26 then
+                                if progress and progress >= 0.30 then
                                     pcall(function()
                                         local events = ReplicatedStorage:FindFirstChild("events")
                                         if events then
